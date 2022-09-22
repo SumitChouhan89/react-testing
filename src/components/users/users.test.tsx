5a41925040bf6dc86a66bcdd6d6df2d218918c7b
Sumit Chauhan <sumit.chouhan@technoidentity.com>
2022-09-22 10:14:08 +0530

    Adds error handling in the mock response

--- src/components/users/users.test.tsx
+++ src/components/users/users.test.tsx
@@ -1,5 +1,7 @@
 import { render, screen } from "@testing-library/react";
+import { server } from "../../mocks/server";
 import { Users } from "./users";
+import { rest } from "msw";
 
 describe("Users Component", () => {
   test("Renders Correctly", () => {
@@ -12,5 +14,19 @@ describe("Users Component", () => {
     render(<Users />);
     const users = await screen.findAllByRole("listitem");
     expect(users).toHaveLength(3);
+  });
+
+  test("Renders error", async () => {
+    server.use(
+      rest.get(
+        "https://jsonplaceholder.typicode.com/users",
+        (req, res, ctx) => {
+          return res(ctx.status(500));
+        }
+      )
+    );
+    render(<Users />);
+    const error = await screen.findByText("Error Fetching Users");
+    expect(error).toBeInTheDocument();
   });
 });
